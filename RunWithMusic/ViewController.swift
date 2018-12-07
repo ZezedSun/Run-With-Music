3d63e3cb90764ad55be950ce5cf3887a3cefe195
SYY <syy@haodemacbook-air.local>
2018-12-07 17:56:36 -0600

    Initial Commit

---
+++ RunWithMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunWithMusic
+//
+//  Created by SYY on 2018/12/7.
+//  Copyright © 2018 Mingze Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
